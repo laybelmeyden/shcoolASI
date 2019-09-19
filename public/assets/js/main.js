1e06262a4836ff3de4b4c3d2ba1bf5620a0a4fb3
laybelmeyden <trycollens@gmail.com>
2019-09-19 23:33:21 +0300

    added/register2

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,7 +22,7 @@ if (window.matchMedia("(max-width: 920px)").matches) {
     shape.setAttribute("viewBox", "-10 300 550 400");
 };
 let url = window.location.href;
-if (url.indexOf('http://127.0.0.1:8000/new') != -1) {
+if (url.indexOf('https://itvolonter.ru/new') != -1) {
     document.querySelector("nav").style.background = "rgba(250, 240, 231, 0.65)";
     document.querySelector("nav").classList.add("nav_12");
 };
